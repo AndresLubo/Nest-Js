c1c422a00036eb28eda4c3b8b31ac58eb3e72e03
Andres Lubo <andresl@sempertex.local>
2022-11-04 10:01:10 -0500

    Modulo global - Clase 8

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,13 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
-
-const API_KEY = '123456789';
-const API_KEY_PROD = 'PROD123456789';
+import { DatabseModule } from './databse/databse.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, HttpModule],
+  imports: [UsersModule, ProductsModule, HttpModule, DatabseModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: 'API_KEY',
-      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
-    },
     {
       provide: 'TASKS',
       useFactory: async (http: HttpService) => {
